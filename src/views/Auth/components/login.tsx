ab72f0b1230885c59c391b4a0233d9ab10dad5d0
sudeeptimalsina <sudeeptimalsina18@gmail.com>
2020-03-19 20:00:03 +0545

    Add social signin feature

--- src/views/Auth/components/login.tsx
+++ src/views/Auth/components/login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import firebase from "../../../firebase";
 import "firebase/auth";
@@ -12,11 +12,67 @@ interface UserData {
 
 const Login = () => {
     const authContext = useContext(AuthContext);
+    const {loadingAuthState} = useContext(AuthContext);
     const history = useHistory();
     const [values, setValues] = useState({
         email: "",
         password: ""
     } as UserData);
+
+    const db = firebase.firestore();
+
+    useEffect(() => {
+        firebase
+        .auth()
+        .getRedirectResult()
+        .then(result => {
+            if (!result || !result.user || !firebase.auth().currentUser) {
+                return;
+            }
+
+            return setUserProfile().then(() => {
+                redirectToTargetPage();
+            });
+        })
+        .catch(error => {
+            console.log(error, 'error');
+        });
+
+    }, []);
+
+    const setUserProfile = async () => {
+        if (await isUserExists()) {
+            return;
+        }
+
+        const currentUser = firebase.auth().currentUser!;
+        db
+        .collection("Users")
+        .doc(currentUser.uid)
+        .set({
+            username: currentUser.displayName
+        })
+        .then(() => {
+            console.log('Saved');
+            return;
+        })
+        .catch(error => {
+            console.log(error.message);
+            alert(error.message);
+        })
+    }
+
+    const isUserExists = async () => {
+        const doc = await db
+            .collection("users")
+            .doc(firebase.auth().currentUser!.uid)
+            .get()
+        return doc.exists;
+    }
+
+    const redirectToTargetPage = () => {
+        history.push("/dashboard");
+    }
 
     const handleClick = () => {
        history.push("/auth/signup") 
@@ -47,6 +103,47 @@ const Login = () => {
         });
     }
 
+    const handleSocialClick = (sns: any) => {
+        console.log(sns, 'sns');
+
+        let provider: firebase.auth.AuthProvider;
+        switch (sns) {
+            case "Facebook":
+                provider = new firebase.auth.FacebookAuthProvider();
+                console.log(provider, 'fbprovider');
+                break;
+            
+            case "Google":
+                provider = new firebase.auth.GoogleAuthProvider();
+                console.log(provider, 'gprovider');
+                break;
+
+            case "Twitter":
+                provider = new firebase.auth.TwitterAuthProvider();
+                break;
+        
+            default:
+                throw new Error("Unsupported SNS" + sns)
+        }
+
+        firebase
+        .auth()
+        .signInWithRedirect(provider)
+        .catch(handleAuthError);
+    }
+
+    const handleAuthError = (error: firebase.auth.Error) => {
+        console.log(error)
+    }
+
+    if (loadingAuthState) {
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>  
+        );
+    }
+
     return (
         <div style={{textAlign: 'center'}}>
             <h1>Login</h1>
@@ -55,8 +152,13 @@ const Login = () => {
                 <input type="password" name="password" value={values.password} placeholder="Enter your Password" onChange={handleChange} /><br /><br />
                 <button>Login</button>
                 <p>Not logged in yet?</p>
-                <button onClick={handleClick}>SignUp</button>
+                <button onClick={handleClick}>SignUp</button> <br/><br/>
             </form>
+
+                <p>Social SignUp</p>
+                <button onClick={() => handleSocialClick("Facebook")}>SignIn with Facebook</button><br/><br/>
+                <button onClick={() => handleSocialClick("Google")}>SignIn with Google</button><br/><br/>
+                <button onClick={() => handleSocialClick("Twitter")}>SignIn with Twitter</button><br/><br/>
         </div>
     );
 }
--- src/views/Dashboard/components/dashboard.tsx
+++ src/views/Dashboard/components/dashboard.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useHistory } from "react-router-dom";
 import firebase from "../../../firebase";
 import "firebase/firestore";
 
 const Dashboard = () => {
+    const [userName, setUserName] = useState();
     const history = useHistory();
 
     const handleClick = (event: any) => {
@@ -17,10 +18,25 @@ const Dashboard = () => {
         })
     }
 
+    useEffect(() => { 
+        const db = firebase.firestore();
+            db
+            .collection("Users")
+            .doc(firebase.auth().currentUser!.uid)
+            .get()
+            .then(res => {
+                const user = res.data();
+                if (user) {
+                    setUserName(user['username'])
+                }
+            })
+    }, []);
+
     return (
         <div style={{textAlign: 'center'}}>
             <h1>Dashboard</h1>
-            <h2>Welcome to Dashboard!</h2><br/><br/>
+            <h2>Welcome to Dashboard!</h2>
+            <h3>{userName}</h3>
             <button onClick={handleClick}>Logout</button>
         </div>
     );
